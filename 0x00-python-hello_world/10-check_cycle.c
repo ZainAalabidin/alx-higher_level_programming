d036f050d941a4a114682dee91d12cc78a8a322f
ZainAalabidin <zynly9303@gmail.com>
2024-01-04 14:29:46 -0800

    hello world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if list has cycle
+ * @list: linked list
+ * Return: 1 if cycle 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *x = *y = list;
+
+	if (list)
+		return (0);
+	while (x && y && y->next)
+	{
+		x = x->next;
+		y = y->next->next;
+		if (y == x)
+			return (1);
+	}
+	return (0);
+}
