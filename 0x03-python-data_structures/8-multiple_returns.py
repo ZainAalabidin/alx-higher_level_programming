df376516ef941c9710aabd70a7d6c45d5c0e9a51
ZainAalabidin <zynly9303@gmail.com>
2024-01-08 15:21:09 -0800

    data struct

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        if len(sentence) < 0:
-              length = 0
-              char = None
-        else:
-              length = len(sentence)
-              char = sentence[0]
-              detail = length, char
-              return detail
+    if len(sentence) < 0:
+        length = 0
+        char = None
+    else:
+        length = len(sentence)
+        char = sentence[0]
+        detail = length, char
+
+        return detail
