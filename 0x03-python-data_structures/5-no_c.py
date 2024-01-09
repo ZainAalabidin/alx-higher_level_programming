9a95caf1aeb01cbe96a2ed4edc7660f5c1664d1d
ZainAalabidin <zynly9303@gmail.com>
2024-01-09 03:04:50 -0800

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        New_list = list(my_string)
-        for str in my_string:
-            if str == "c" or str == "C":
-                New_list.remove(str)
-        return "".join(New_list)
+    New_list = list(my_string)
+    for str in my_string:
+        if str == "c" or str == "C":
+            New_list.remove(str)
+    return ("".join(New_list))
