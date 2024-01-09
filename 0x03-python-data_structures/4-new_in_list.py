48855de8d46c79a98e9e9234e69feee7dd5fd126
ZainAalabidin <zynly9303@gmail.com>
2024-01-09 02:35:45 -0800

    data structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new_list = []
     index = len(my_list) - 1
+    new_list = my_list.copy()
 
     if idx < 0 or idx > index:
         return my_list
     else:
-        my_list[idx] = element
-        return my_list
+        new_list[idx] = element
+        return new_list
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     NewStr = ""
-    if my_string:
-        for str in my_string:
-            if str != "c" and str != "C":
-                NewStr += str
-            return NewStr
+    for str in my_string:
+        if str != "c" and str != "C":
+            NewStr += str
+        return NewStr
