834a343c73e858dc60cbce22915f291b7ccd71be
ZainAalabidin <zynly9303@gmail.com>
2024-01-09 05:14:15 -0800

    data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,7 @@ def multiple_returns(sentence):
     if len(sentence) < 0:
         length = 0
         char = None
+        detail = length, char
     else:
         length = len(sentence)
         char = sentence[0]
