1f3ec4cee5031aab30e9833c10ecaab374d4d788
ZainAalabidin <zynly9303@gmail.com>
2024-01-09 12:47:02 -0800

    c linked list

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * palindrome - check if is palindrome with recursion
+ * @left: point to the left node
+ * @right: point to the right node
+ *
+ * Return: 1 palindrome, 0 not palindrome
+ */
+int palindrome(listint_t **left, listint_t *right)
+{
+	int result;
+
+	if (right != NULL)
+	{
+		result = palindrome(left, right->next);
+		if (result != 0)
+		{
+			result = (right->n == (*left)->n);
+			*left = (*left)->next;
+			return (result);
+		}
+		return (0);
+
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of linked list
+ *
+ * Return: 1 palindrome, 0 not palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+	{
+		return (0);
+	}
+	return (palindrome(head, *head));
+}
