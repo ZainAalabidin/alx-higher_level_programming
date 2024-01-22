ad68564477cb43b0c2ff9fa1b34c559d60fca92d
ZainAalabidin <zynly9303@gmail.com>
2024-01-23 00:30:03 +0200

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,8 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    leng = 0
-    for t in range(x):
+    lengt = 0
+    for t in range(0, x):
         try:
             print("{:d}".format(my_list[t]), end="")
             leng = leng + 1
