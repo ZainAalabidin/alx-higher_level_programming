4522d6abac675447c70b9239576d453b8f595226
ZainAalabidin <zynly9303@gmail.com>
2024-01-23 00:06:07 +0200

    exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    length = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            length += 1
+        except:
+            continue
+    print()
+    return length
