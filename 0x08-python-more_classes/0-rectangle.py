5c54167efcb67a18a0a2d8ff1a74fd1712b6877b
ZainAalabidin <zynly9303@gmail.com>
2024-01-31 22:35:43 +0200

    classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""0-rectangle, built for project 0x08 task 0.
+"""
+
+
 class Rectangle:
+    """Empty class , will be built upon in later tasks.
+    """
     pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,44 @@
 #!/usr/bin/python3
+"""1-rectangle, built for project 0x08 task 1.
+"""
+
+
 class Rectangle:
+    """At this stage the class creates private instance attributes .
+
+    Args:
+        width (int): horizontal dimension of rectangle, defaults to 0.
+        height (int): vertical dimension of rectangle, defaults to 0.
+    
+    """
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
 
     @property
     def height(self):
+        """__height getter.
+
+        Return:
+            __height (int): vertical dimension of rectangle
+
+        """
         return self.height
     
     @height.setter
     def height(self, value):
+        """Args:
+            value (int): vertical dimension of rectangle
+
+        Attributes:
+            __height (int): vertical dimension of rectangle.
+
+        Raises:
+            TypeError: if value is not an int.
+            ValueErroe: if value is less than 0.
+
+        """
         if isinstance(value, int):
             self.__height = value
         else:
@@ -20,10 +49,27 @@ class Rectangle:
         
     @property
     def width(self):
+        """__width getter.
+
+        Return:
+            __width (int): horizontal dimension of rectangle
+
+        """
         return self.width
     
     @width.setter
     def width(self, value):
+        """Args:
+            value (int): horizontal dimension of rectangle
+
+        Attributes:
+            __height (int): horizontal dimension of rectangle.
+
+        Raises:
+            TypeError: if value is not an int.
+            ValueErroe: if value is less than 0.
+            
+        """
         if isinstance(value, int):
             self.__width = value
         else:
