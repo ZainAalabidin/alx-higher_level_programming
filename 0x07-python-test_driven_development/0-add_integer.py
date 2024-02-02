51400bf41acbbec52b8012eff2911e7de0c34c19
ZainAalabidin <zynly9303@gmail.com>
2024-02-02 16:47:30 +0200

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ Attributes:
     add_integer: function that adds two integers.
 """
 
+
 def add_integer(a, b=98):
     """Adds tow integer and/or float values.
 
@@ -20,13 +21,13 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    
+
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    
+
     if isinstance(a, float):
         a = int(a)
-    
+
     if isinstance(b, float):
         b = int(b)
 
