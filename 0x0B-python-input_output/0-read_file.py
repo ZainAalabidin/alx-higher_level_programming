5fad0bf6528beb3bc3d85c8329e6e17133d198c4
ZainAalabidin <zynly9303@gmail.com>
2024-02-07 00:47:09 +0200

    input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Method that read files"""
     with open(filename, "r", encoding="utf-8") as f:
         for line in f:
-            print(line, end="")
+            print(line)
