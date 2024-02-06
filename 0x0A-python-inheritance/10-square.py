bd5dffbe12360ebbd0cb5e68070a7eb04f076146
ZainAalabidin <zynly9303@gmail.com>
2024-02-06 18:29:32 +0200

    inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Import `9-rectangle` moduke"""
+"""Import `9-rectangle` module"""
 Rectangle = __import__("9-rectangle").Rectangle
 
 
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,7 +6,7 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size):
-        """MMethod for initilized Square class"""
+        """Method for initilized Square class"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
