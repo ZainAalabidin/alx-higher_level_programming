e8c7da5f737883db3dec0ab950b0e29c5869ae46
ZainAalabidin <zynly9303@gmail.com>
2024-02-06 04:27:56 +0200

    inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""Define module with rectange class"""
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """ "Initilized rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""Define module with rectange class"""
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """ "Initilized rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Method that returns area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
