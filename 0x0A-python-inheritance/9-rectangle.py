636f42bf88281ac414c6cc162c402eddce141132
ZainAalabidin <zynly9303@gmail.com>
2024-02-06 18:03:55 +0200

    inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""Rectangle class Module"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
-"""Define module with rectange class"""
 
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
 
     def __init__(self, width, height):
-        """ "Initilized rectangle"""
+        """Initilize rectangle method"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
