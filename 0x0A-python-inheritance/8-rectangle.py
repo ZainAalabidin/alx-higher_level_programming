62937e54ccd5f97c925639f78de8a4de82f0d725
ZainAalabidin <zynly9303@gmail.com>
2024-02-06 04:37:21 +0200

    inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,9 +5,11 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
+
     def __init__(self, width, height):
         """ "Initilized rectangle"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+        def __init__(self, width, height)
+            self.integer_validator("width", width)
+            self.__width = width
+            self.integer_validator("height", height)
+            self.__height = height
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,12 +5,14 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
+
     def __init__(self, width, height):
         """ "Initilized rectangle"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+        def __init__(self, width, height)
+            self.integer_validator("width", width)
+            self.__width = width
+            self.integer_validator("height", height)
+            self.__height = height
 
     def area(self):
         """Method that returns area of rectangle"""
