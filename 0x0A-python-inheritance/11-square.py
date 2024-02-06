5b0736263d758335272f59bf3562500749e9e0eb
ZainAalabidin <zynly9303@gmail.com>
2024-02-06 18:22:44 +0200

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,14 +6,14 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size):
-        """Initilized Square class"""
+        """MMethod for initilized Square class"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
         """Method that returns area of rectangle"""
-        return self.__size * self.__size
+        return self.__size ** 2
 
     def __str__(self):
         """Method that returns a string"""
