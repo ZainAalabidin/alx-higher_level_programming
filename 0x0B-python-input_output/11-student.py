e7fd1908952648c90c25dc87c7e011b6dcece154
ZainAalabidin <zynly9303@gmail.com>
2024-02-07 04:49:03 +0200

    input output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""`9-student module"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialized method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """method that return dictionary of the class"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """Method that return value for specific key"""
+        if attrs is not None:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Method that return values of json file from path"""
+        return {k: v for k, v in self.__dict__.items() if k in json}
