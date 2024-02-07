41c2aea2d07387a4193adeedf8b95d6479e2aa57
ZainAalabidin <zynly9303@gmail.com>
2024-02-07 03:58:40 +0200

    input output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""`9-student module"""
+
 
 class Student:
+    """Student class"""
+
     def __init__(self, first_name, last_name, age):
+        """Initialized method"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """method that return dictionary of the class"""
         return self.__dict__
-    
