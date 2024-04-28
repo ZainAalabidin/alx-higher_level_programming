1ad3d93847582e7fdad57cf87648b670111292f3
ZainAlabidin <zynly9303@gmail.com>
2024-04-28 20:08:47 +0300

    ok

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,5 +2,6 @@
 # Variables for email and subject
 email="test@gmail.com"
 subject="I will always be here for PLD"
+
 # Send a POST request to the URL with the specified variables and display the body of the response
 curl -s -d "email=$email" -d "subject=$subject" "$1"
