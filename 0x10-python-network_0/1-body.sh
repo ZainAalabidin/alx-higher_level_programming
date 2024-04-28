be83247515eacd19dc1e219f58bafefff734676e
ZainAlabidin <zynly9303@gmail.com>
2024-04-28 19:00:12 +0300

    ok

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -sI "$1" | grep -i Content-Length | wc -c
-~                                               
+# Takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
