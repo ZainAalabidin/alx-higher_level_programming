8e05de4c56d816eeb0c463dc4e6059d343056c7e
ZainAlabidin <zynly9303@gmail.com>
2024-04-29 00:03:43 +0300

    ok

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response. no pipe
-awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
+# Send a HEAD request to the URL and store the response headers in a variable
+response_headers=$(curl -s -I "$1")
+
+# Extract the status code from the response headers
+status_code=$(echo "$response_headers" | head -n 1 | awk '{print $2}')
+
+# Display the status code
+echo "Status code: $status_code"
