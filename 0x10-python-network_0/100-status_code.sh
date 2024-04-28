a12af7be3fff50afffd6fc1bec19c1d1558d2d1f
ZainAlabidin <zynly9303@gmail.com>
2024-04-29 00:06:43 +0300

    ok

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Send a HEAD request to the URL and store the response headers in a variable
-response_headers=$(curl -s -I "$1")
-
-# Extract the status code from the response headers
-status_code=$(echo "$response_headers" | head -n 1 | awk '{print $2}')
-
-# Display the status code
-echo "Status code: $status_code"
+# sends a request to a URL, and displays only the status code of the response.
+curl -s -o /dev/null -w "%{http_code}" "$1"
