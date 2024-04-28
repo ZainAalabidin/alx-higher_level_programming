f445ff3fac7eb48aeba1586d3d8977c4f197ff6f
ZainAlabidin <zynly9303@gmail.com>
2024-04-29 00:00:33 +0300

    ok

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response. no pipe
-curl -s -I "$1" | head -n 1 | awk '{print $2}'
+curl -s -o /dev/null -w "%{http_code}" "$1"
