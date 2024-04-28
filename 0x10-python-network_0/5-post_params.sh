e25eccf594d18f6e58a6047a976c6f45eadf2d3c
ZainAlabidin <zynly9303@gmail.com>
2024-04-28 20:10:29 +0300

    ok

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Variables for email and subject
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
 # Send a POST request to the URL with the specified variables and display the body of the response
-curl -s -d "email=$email" -d "subject=$subject" "$1"
+curl -s -d "email="test@gmail.com" -d "subject=I will always be here for PLD" "$1"
