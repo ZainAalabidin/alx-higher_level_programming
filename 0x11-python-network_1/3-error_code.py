bfebb69a2ba4612b662f9eb6a1f5dfad0e2c7eed
ZainAlabidin <zynly9303@gmail.com>
2024-04-30 21:24:44 +0300

    ok

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ 
-script that takes in a URL, 
-sends a request to the URL and displays the body of the response 
+Script that takes in a URL, 
+sends a request to the URL, and displays the body of the response 
 """
 import urllib.request
 import urllib.error
 import sys
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
