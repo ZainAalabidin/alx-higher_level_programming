3820f265c2331da8ef1a5a6a3a90374aed1a148c
ZainAlabidin <zynly9303@gmail.com>
2024-04-30 19:47:07 +0300

    ok

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        resp = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(resp)))
+        print("\t- content: {}".format(resp))
+        print("\t- utf8 content: {}".format(resp.decode("utf-8")))
+
