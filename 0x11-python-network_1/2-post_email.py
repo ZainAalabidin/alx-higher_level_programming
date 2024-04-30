07476b992e68c9271992115576070cb6c3398f77
ZainAlabidin <zynly9303@gmail.com>
2024-04-30 20:56:16 +0300

    ok

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email, 
-
-sends a POST request to the passed URL with the email as a parameter, 
+Script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response
 """
 import urllib.request
 import urllib.parse
 import sys
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
