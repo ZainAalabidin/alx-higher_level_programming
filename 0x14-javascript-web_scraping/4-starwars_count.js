a42e88d316c3cd790ef5409c102d2bab862e9e79
ZainAlabidin <zynly9303@gmail.com>
2024-06-05 18:41:52 +0300

    ok

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,11 +10,7 @@ request(apiUrl, (err, response, body) => {
     let count = 0;
 
     films.forEach(film => {
-      if (
-        film.characters.includes(
-          `https://swapi-api.alx-tools.com/api/people/${characterId}/`
-        )
-      ) {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
         count++;
       }
     });
