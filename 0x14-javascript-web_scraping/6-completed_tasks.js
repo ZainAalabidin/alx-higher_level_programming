0328b26178005e96b31708494278e28b7a593ddc
ZainAlabidin <zynly9303@gmail.com>
2024-06-06 01:06:40 +0300

    ok

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,19 +6,19 @@ request(url, (error, Response, body) => {
   // Make an HTTP GET request to the specified URL.
   if (!error) {
     const toDos = JSON.parse(body); // Parse the JSON response body into a JavaScript object.
-    const completed_tasks = {}; // Initialize an object to keep track of completed tasks by user ID.
+    const completedTasks = {}; // Initialize an object to keep track of completed tasks by user ID.
 
     toDos.forEach(toDo => {
       // Iterate through each to-do item.
       if (toDo.completed) {
         // Check if the to-do item is completed.
-        if (!completed_tasks[toDo.userId]) {
+        if (!completedTasks[toDo.userId]) {
           // If the user ID is not yet in the completed_tasks object,
-          completed_tasks[toDo.userId] = 0; // initialize it with a value of 0.
+          completedTasks[toDo.userId] = 0; // initialize it with a value of 0.
         }
-        completed_tasks[toDo.userId] += 1; // Increment the count of completed tasks for the user.
+        completedTasks[toDo.userId] += 1; // Increment the count of completed tasks for the user.
       }
     });
-    console.log(completed_tasks); // Log the completed tasks object to the console.
+    console.log(completedTasks); // Log the completed tasks object to the console.
   }
 });
