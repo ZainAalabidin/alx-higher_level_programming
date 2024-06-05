cbdec769e2d86dd883c3bc5daa490e67e8516d33
ZainAlabidin <zynly9303@gmail.com>
2024-06-05 18:25:22 +0300

    ok

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,17 +3,8 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = '18';
 
-if (!apiUrl) {
-  console.log('Usage: ./script.js <API_URL>');
-  process.exit(1);
-}
-
 request(apiUrl, (err, response, body) => {
-  if (err) {
-    console.error(`An error occurred: ${err}`);
-  } else if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data: ${response.statusCode}`);
-  } else {
+  if (!err) {
     const data = JSON.parse(body);
     const films = data.results;
     let count = 0;
