d92b3cab7813a9c1cc59a714bf9de6688b3a91d2
ZainAlabidin <zynly9303@gmail.com>
2024-06-06 01:04:35 +0300

    ok

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,14 +2,18 @@
 const request = require('request'); // Import the 'request' module for making HTTP requests.
 const url = process.argv[2]; // Get the URL from the command line arguments.
 
-request(url, (error, Response, body) => { // Make an HTTP GET request to the specified URL.
+request(url, (error, Response, body) => {
+  // Make an HTTP GET request to the specified URL.
   if (!error) {
     const toDos = JSON.parse(body); // Parse the JSON response body into a JavaScript object.
     const completed_tasks = {}; // Initialize an object to keep track of completed tasks by user ID.
 
-    toDos.forEach(toDo => { // Iterate through each to-do item.
-      if (toDo.completed) { // Check if the to-do item is completed.
-        if (!completed_tasks[toDo.userId]) { // If the user ID is not yet in the completed_tasks object,
+    toDos.forEach(toDo => {
+      // Iterate through each to-do item.
+      if (toDo.completed) {
+        // Check if the to-do item is completed.
+        if (!completed_tasks[toDo.userId]) {
+          // If the user ID is not yet in the completed_tasks object,
           completed_tasks[toDo.userId] = 0; // initialize it with a value of 0.
         }
         completed_tasks[toDo.userId] += 1; // Increment the count of completed tasks for the user.
